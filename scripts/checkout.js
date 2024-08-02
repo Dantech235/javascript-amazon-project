a9c1b015e81b41115a3cceacf3764209a1232e51
Daniel-Ezuko <dezeugo8@gmail.com>
2024-08-02 10:16:32 +0100

    Delete product from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,9 @@ cart.forEach((cartItem) => {
   cartSummaryHTML =
     cartSummaryHTML +
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
     <div class="delivery-date">Delivery date: Wednesday, June 15</div>
 
     <div class="cart-item-details-grid">
@@ -98,7 +100,11 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", function () {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
 
